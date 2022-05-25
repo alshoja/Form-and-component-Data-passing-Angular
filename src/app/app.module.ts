cd1e4df8c5b170a11a23fbc0e1e8daebcfa75175
alshoja <alshoja@gmail.com>
2022-05-25 13:03:48 +0530

    feat: header and footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ErrorComponent } from './error/error.component';
+import { HeaderComponent } from './core/components/header/header.component';
+import { FooterComponent } from './core/components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
       HomeComponent,
-      ErrorComponent
+      ErrorComponent,
+      HeaderComponent,
+      FooterComponent
    ],
   imports: [
     BrowserModule,
