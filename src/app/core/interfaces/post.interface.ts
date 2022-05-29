8d4edc11642dd71b04b9681ec3f867b5fe79b9ab
alshoja <alshoja@gmail.com>
2022-05-29 17:37:07 +0530

    fix: test case 100 %

--- src/app/core/interfaces/post.interface.ts
+++ src/app/core/interfaces/post.interface.ts
@@ -1,8 +1,8 @@
-export interface Post{
+export interface Post {
   userId: number;
   id: number;
-  title: string;
-  body: string;
+  title?: string;
+  body?: string;
 }
 
 
--- src/app/home/tests/home.component.spec.ts
+++ src/app/home/tests/home.component.spec.ts
@@ -1,9 +1,9 @@
 /* tslint:disable:no-unused-variable */
 import { HttpClientModule } from '@angular/common/http';
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, tick } from '@angular/core/testing';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterTestingModule } from '@angular/router/testing';
-import { of } from 'rxjs';
+import { of, throwError } from 'rxjs';
 import { PostsService } from 'src/app/core/services/posts.service';
 import { SharedService } from 'src/app/core/services/shared.service';
 import { PostComponent } from 'src/app/post/post.component';
@@ -21,10 +21,13 @@ describe('HomeComponent', () => {
     "body": "consectetur animi nesciunt "
   }
 
+  const spyPayLoad2 = {
+    "userId": 1,
+    "id": 9,
+  }
+
   let postsServiceSpy = jasmine.createSpyObj('PostsService', ['getPost']);
   let dataServiceSpy = jasmine.createSpyObj('SharedService', ['sendPostToComponent']);
-
-  postsServiceSpy.getPost.and.returnValue(of(spyPayLoad));
   dataServiceSpy.sendPostToComponent.and.returnValue(of(spyPayLoad));
 
   beforeEach(async(() => {
@@ -79,11 +82,48 @@ describe('HomeComponent', () => {
     const formData = {
       "id": 2,
     };
-
+    postsServiceSpy.getPost.and.returnValue(of(spyPayLoad));
     component.form.setValue(formData);
     component.onSubmit();
     expect(postsServiceSpy.getPost).toHaveBeenCalledWith(formData.id);
     expect(dataServiceSpy.sendPostToComponent).toHaveBeenCalledWith(spyPayLoad);
+    expect(component.errorState).toBe(false);
+    expect(component.errorMessage).toBe('');
+  });
+
+  it('should not get a post when body or title is missing', () => {
+    const formData = {
+      "id": 2,
+    };
+    postsServiceSpy.getPost.and.returnValue(of(spyPayLoad2));
+    component.form.setValue(formData);
+    component.onSubmit();
+    expect(postsServiceSpy.getPost).toHaveBeenCalledWith(formData.id);
+    expect(component.errorState).toBe(true);
+    expect(component.errorMessage).toBe('Either Body or Title is Missing');
+  });
+
+
+  it('should through error message on 404 status', () => {
+    const formData = {
+      "id": 2,
+    };
+    postsServiceSpy.getPost.and.returnValue(throwError({ status: 404, error: { message: 'some message' } }));
+    component.form.setValue(formData);
+    component.onSubmit();
+    expect(component.errorState).toBe(true);
+    expect(component.errorMessage).not.toBe('');
+  });
+
+  it('should through message on server is not up', () => {
+    const formData = {
+      "id": 2,
+    };
+    postsServiceSpy.getPost.and.returnValue(throwError({ status: 500 }));
+    component.form.setValue(formData);
+    component.onSubmit();
+    expect(component.errorState).toBe(true);
+    expect(component.errorMessage).toBe('Internal Server Error Or Something went wrong!');
   });
 
 });
